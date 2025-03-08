e513f0924b87348944e9b1932eb6235c3d89f67d
AtulKumar2004 <akdeoghar16@gmail.com>
2025-03-08 18:52:05 +0530

    New js Updates

--- index.js
+++ index.js
@@ -49,10 +49,11 @@ app.use(passport.initialize());
 app.use(passport.session());
 
 const db = new pg.Client({
-    connectionString: process.env.DATABASE_URL, // ✅ Use Render's DATABASE_URL
-    ssl: {
-        rejectUnauthorized: false, // ✅ Required for Render PostgreSQL
-    },
+    user: process.env.PG_USER,
+    host: process.env.PG_HOST,
+    database: process.env.PG_DATABASE,
+    password: process.env.PG_PASSWORD,
+    port: process.env.PG_PORT,
 });
 db.connect();
 
